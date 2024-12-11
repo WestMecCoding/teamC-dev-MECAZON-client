9c18c9c4f74d9ffaeface387f8e36f85d6d3b2d1
Lillian Jensen <ljense251@west-mec.org>
2024-12-11 13:49:46 -0700

    updated files

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Groceries from "./pages/Groceries";
+import Admin from "./pages/Admin";
 import "./App.css";
 export default function App() {
   return (
